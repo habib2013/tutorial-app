6a0b22ab2345004078031aff8421b3090a64a832
habib2013 <nairabet25@gmail.com>
2020-04-24 13:33:59 +0100

    Complete React Tutorial

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        This is the first page,i can change it anytime
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Components/Component1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.js';
+
+function Component1() {
+  return (
+    <div>
+     This is the begining of React 
+
+    </div>
+  );
+}
+
+export default Component1;
