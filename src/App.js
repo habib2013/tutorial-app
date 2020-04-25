2f569bd00d68f7da132e59da00d50132464e6ed1
habib2013 <nairabet25@gmail.com>
2020-04-25 12:10:23 +0100

    Day 2 of React Course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getMovies } from "./services/fakeMovieService";
 
 function App() {
+
+  let myMovie = getMovies();
+
+  const handleDelete = (movie) => {
+    console.log(movie);
+
+  } 
+
+const myRenderer = () => {
+  return (myMovie.length === 0) ? 'No Movie' : myMovie.length + " Available";
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        This is the first page,i can change it anytime
-         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<main className="container">
+
+<h1>{myRenderer()}</h1>
+
+<table className="table">
+ <thead>
+<th>title</th>
+<th>Genre</th>
+<th>Stock</th>
+<th>Rate</th>
+<th></th>
+ </thead>
+ <tbody>
+
+{myMovie.map(movie => 
+  <tr key={movie._id}> 
+    <td>{movie.title}</td>
+    <td>{movie.genre.name}</td>
+    <td>{movie.numberInStock}</td>
+    <td>{movie.dailyRentalRate}</td>
+    <td className="btn btn-danger" onClick={() => handleDelete(movie)}> DELETE</td>
+  </tr>
+  
+  )} 
+   <td>
+
+ 
+   </td>
+ </tbody>
+
+</table>
+
+</main>
   );
 }
 
--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 class Counter extends Component {
    state = { 
-      count : 0,
+      count : 5,
+      tags:['tag1','tag2','tag3']
+    };
+// handleIncrement(){
+//    console.log("Increment Clicked")
+// }
+myBtnIncrement = product => {
+   // console.log("Increment Clicked",this);
+   console.log(product)
+   this.setState({count: this.state.count + 1});
+} 
+
+dohandleIncrement =() => {
+   this.myBtnIncrement({id:1});
+}
+
+
+
+    renderTags(){
+       if(this.state.tags.length === 0) return <p>There are no Tags</p>;
+       return <ul> 
+       {this.state.tags.map(tag => 
+          <li key={tag}>{ tag }</li>
+       )}
+      </ul>
     }
    render() { 
+    
       return ( 
          <div>
-         <p className="badge badge-danger">  {this.myIncrement()}</p> 
+         <p className={this.getBadgeClasses()}>  {this.myIncrement()}</p> 
+         <button onClick={() => this.myBtnIncrement({id:1})} className="btn btn-primary">Increment</button>
+        
+        {this.state.tags.length === 0 && 'Please Create a new Tag'}
+        {this.renderTags()}
          </div>
        );
+   }
+
+
+
+   getBadgeClasses() {
+      let classes = "badge m-2 badge-";
+      classes += (this.state.count === 0) ? 'danger' : 'primary';
+      return classes;
    }
 
    myIncrement(){
