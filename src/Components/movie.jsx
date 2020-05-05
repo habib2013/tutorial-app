f4767a1aa871ff41761bd75d6bd3f868e2bf8748
habib2013 <nairabet25@gmail.com>
2020-05-05 10:42:22 +0100

    This is my react project

--- src/Components/movie.jsx
+++ src/Components/movie.jsx
@@ -1,59 +1,51 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
+import { getMovies } from '../services/fakeMovieService'
 
-export class Movie extends Component {
-   state = {
-movies : getMovies()
-     };
-   
-     myRenderer = () => {
-      return (this.state.movies.length === 0) ? 'No Movie' : this.state.movies.length + " Available";
-    }   
-  
-     handleDelete = (movie) => {
-      console.log(movie);
-  const movies = this.state.movies.filter(m => m._id !== movie._id);   
-  this.setState({movies});
-  
-    } 
+class Movie extends Component {
+   state = { 
+      movies:getMovies(),
+    };
 
+deleteMovie = (movie) => {
+   const movies = this.state.movies.filter(m => m._id !== movie._id);
+   this.setState({movies});
+// console.log(product);
+}
+
+handleLenght = () => {
+  return (this.state.movies.length === 0) ? 'No movie here' : this.state.movies.length + " Found" 
+}
+    
    render() { 
-      return (  
-<main className="container">
+      return ( 
 
-<h1>{this.myRenderer()}</h1>
-
+<div>
+<h3>{this.handleLenght()}</h3>
 <table className="table">
- <thead>
-<th>title</th>
-<th>Genre</th>
-<th>Stock</th>
-<th>Rate</th>
-<th></th>
- </thead>
- <tbody>
-
-{this.state.movies.map(movie => 
-  <tr key={movie._id}> 
-    <td>{movie.title}</td>
-    <td>{movie.genre.name}</td>
-    <td>{movie.numberInStock}</td>
-    <td>{movie.dailyRentalRate}</td>
-    <td className="btn btn-danger" onClick={() => this.handleDelete(movie)}> DELETE</td>
-  </tr>
-  
-  )} 
-   <td>
-
- 
-   </td>
- </tbody>
-
+   <tr>
+      <th>Title</th>
+      <th>Genre</th>
+      <th>Stock</th>
+      <th>Rate</th>
+      <th></th>
+   </tr>
+   <tbody>
+    
+         {this.state.movies.map(item => 
+      <tr key={item._id}>
+  <td >{item.title}</td>
+         <td >{item.genre.name}</td>
+         <td >{item.numberInStock}</td> 
+         <td >{item.dailyRentalRate}</td> 
+          <td><button className="btn btn-danger" onClick={()=> this.deleteMovie(item)}>DELETE</button></td>  
+            </tr>
+         )}
+   </tbody>
 </table>
 
-</main>
-
-      );
+</div>
+       );
    }
 }
  
+export default Movie;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Counter from './Components/counter';
-import { Movie } from "./Components/movie";
+import Movie from './Components/movie';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css'
