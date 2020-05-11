0388ac58079d034a42c40527c17005b6b51b2ae6
habib2013 <nairabet25@gmail.com>
2020-05-11 10:27:58 +0100

    --- Auto-Git Commit ---

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 class Counter extends Component {
    state = { 
-      value : this.props.value,
+      value : this.props.counter.value,
     
     };
 // handleIncrement(){
 //    console.log("Increment Clicked")
 // }
-myBtnIncrement = product => {
+myBtnIncrement = () => {
    // console.log("Increment Clicked",this);
-   console.log(product)
-   this.setState({count: this.state.value + 1});
+   // console.log(product)
+   this.setState({value: this.state.value + 1});
 } 
 
 dohandleIncrement =() => {
@@ -24,8 +24,8 @@ dohandleIncrement =() => {
          <div>
       
          <p className={this.getBadgeClasses()}>  {this.myIncrement()}</p> 
-         <button onClick={() => this.myBtnIncrement({id:1})} className="btn btn-primary">Increment</button>
-        
+         <button onClick={this.myBtnIncrement} className="btn btn-primary">Increment</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2"> <i className="fa fa-trash"></i></button>
    
          </div>
        );
