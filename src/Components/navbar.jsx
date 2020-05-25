050280a7a15d533fc9ed963f88798fea52a36b8b
habib2013 <nairabet25@gmail.com>
2020-05-25 18:19:06 +0100

    --- Auto-Git Commit ---

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -5,7 +5,12 @@ class NavBar extends Component {
    render() { 
       return ( 
 <nav className="navbar navbar-light bg-light">
-         <a href="/" className="navbar-brand">Navbar</a>
+         <a href="/" className="navbar-brand">
+Navbar <span className="badge badge-pill badge-secondary">
+   {this.props.totalCounters}
+</span>
+
+         </a>
      
 
 </nav>
