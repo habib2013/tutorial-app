961cf9496818f69f604fd32ccf640b2b3386fa0a
habib2013 <nairabet25@gmail.com>
2020-06-23 17:07:43 +0100

    restarted after watching the whole tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React,{Component} from 'react';
 import './App.css';
 // import { getMovies } from "./services/fakeMovieService";
-import NavBar from "./Components/navbar";
-import Counters from './Components/counters';
+
 
 class App extends Component {
-  state = {  
-    counters: [
-       {id:1,value:4},
-       {id:2,value:7},
-       {id:3,value:0},
-       {id:4,value:9},
-    ]
- };
+
 
 constructor(){
   super();
@@ -25,50 +17,15 @@ console.log('App - Mounted');
 }
 
 
-handleIncrement = counter => {
-const counters = [...this.state.counters];
-const index = counters.indexOf(counter);
-counters[index] = {...counter};
-counters[index].value++;
-this.setState({counters});
-
-}
-
-handleDecrement = counter => {
-  const counters = [...this.state.counters];
-  const index = counters.indexOf(counter);
-  counters[index] = {...counter};
-  counters[index].value--;
-  this.setState({counters});
-  
-  }
-
-handleDelete = (counterId) => {
-const counters = this.state.counters.filter(c => c.id !== counterId);
-this.setState({counters});
-}
-handleReset = () => {
-const counters = this.state.counters.map(c => {
-    c.value = 0;
-    return c;
- });
- this.setState({counters});
-}
 
   render() { 
     console.log('App - Rendered');
   
     return (
       <React.Fragment>
-<NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+
 <main className="container">
-<Counters
-counters = {this.state.counters}
- onReset={this.handleReset}
-onIncrement = {this.handleIncrement}
-onDecrement = {this.handleDecrement}
-onDelete = {this.handleDelete}
-/>  
+<div>Hi Man</div>
 </main>
 </React.Fragment>
       );
--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import Counter from './counter';
+
 
 class Counters extends Component {
-  
+   state = { 
+      count: 0,
+      tags: [],
+    }
+
+    getClassColor = () => {
+      let classes = 'm-2 badge badge-';
+      classes += this.state.count === 0 ? 'warning' : 'info';
+      return classes;
+      }
+      
+         countState = () => {
+            return (this.state.count === 0) ? 'Zero' : this.state.count;
+         }
+
+         renderTags = () => {
+            return (this.state.tags.length === 0) ? <p>There are No Tags</p> : <ul>
+            {this.state.tags.map(item => <li>item</li>)}
+            </ul>
+         }
+handleClick = () => {
+this.setState({count: this.state.count + 1});
+}
+
 
    render() { 
-      return ( 
-         <div>
-           
-             <button  onClick = {this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
- {this.props.counters.map(counter => 
- <Counter
-  
- key={counter.id} 
- onDelete={this.props.onDelete} 
- onIncrement = {this.props.onIncrement}
- onDecrement = {this.props.onDecrement}
-counter = {counter} />
-    )}
-         </div>
-     
-       );
+      return (
+        
+<div className="container">
+<span className={this.getClassColor()}>{this.countState()}</span>
+<button className="btn btn-primary" onClick={this.handleClick}>Increment</button>
+
+      <span>{this.state.tags.length === 0 && 'Please create a new tag'}</span>
+      <div>{this.renderTags()}</div>
+
+   </div>
+
+      );
    }
+
+
 }
  
 export default Counters;
